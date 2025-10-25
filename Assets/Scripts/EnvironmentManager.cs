62ced17a29dda31c7feb55e3ad0dac17ba1a9f5b
PresenceOP-Coder <shreyasprajapati1230@gmail.com>
2025-10-25 23:16:10 +0530

    feat(UI & Gameplay): Add level-specific backgrounds and scorer logic

---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    [Header("Assign skyboxes for levels")]
+    public Material level1Sky;
+    public Material level4Sky;
+    public Material level5Sky;
+
+    void Start()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+
+        switch (index)
+        {
+            case 1:
+                RenderSettings.skybox = level1Sky;
+                break;
+            case 4:
+                RenderSettings.skybox = level4Sky;
+                break;
+            case 5:
+                RenderSettings.skybox = level5Sky;
+                break;
+            default:
+                Debug.Log("No specific skybox assigned for this scene.");
+                break;
+        }
+
+        DynamicGI.UpdateEnvironment(); // refresh lighting
+    }
+}
